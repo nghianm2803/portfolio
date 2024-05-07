4e8a6a6562fbbb03d1a0ccb78611a794e9937326
Nghia <nghianm2803@gmail.com>
2024-05-07 21:14:30 +0700

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "next-themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NghiaNM - Portfolio",
+  title: "NghiaNM",
   description: "My Portfolio",
 };
 
